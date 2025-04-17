e486cadcc21ebb88abdb7c96179951ac5439fc2a
HYONSU <admin@hyonsu.com>
2025-04-17 15:10:56 +0900

    env에서 사용할 스킨 지정

--- vite.config.js
+++ vite.config.js
@@ -1,71 +1,75 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+import { fileURLToPath, URL } from 'url'
 import path from 'path'
 import fs from 'fs'
 import crypto from 'crypto'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
 import skinRawLoaderPlugin from './plugins/skinRawLoader'
 import metadata from './plugins/metadata'
 
-const skin = process.env.SKIN_NAME || fs.readdirSync('./skins')[0]
-
-const srcPath = fileURLToPath(new URL('./src', import.meta.url))
-
-const commitIds = {
-  frontend: 'fedummy',
-  skin: 'skindummy'
-}
-const versionHeader = crypto.createHash('md5')
-    .update(skin + JSON.stringify(commitIds))
-    .digest('hex')
-    .slice(0, 17)
-
 // https://vite.dev/config/
-export default defineConfig(({ isSsrBuild }) => ({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    skinRawLoaderPlugin(skin),
-    ...(isSsrBuild ? [] : [
-      metadata({ skin, versionHeader, commitIds })
-    ])
-  ],
-  define: {
-    __THETREE_COMMIT_IDS__: JSON.stringify(commitIds),
-    __THETREE_VERSION_HEADER__: JSON.stringify(versionHeader)
-  },
-  base: process.env.NODE_ENV === 'production' ? `/skins/${skin}` : '/',
-  publicDir: isSsrBuild ? false : 'public',
-  resolve: {
-    extensions: ['.js', '.vue'],
-    alias: {
-      // vue: 'vue/dist/vue.esm-bundler.js',
-      '@': srcPath,
-      '~': srcPath,
-      'skin': path.resolve('./skins', skin),
+export default defineConfig(({ mode, isSsrBuild }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-      'vuex': path.resolve(srcPath, 'main'),
-    },
-  },
-  server: {
-    proxy: {
-      '/internal': {
-        target: process.env.API_HOST || 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        format: isSsrBuild ? 'cjs' : 'esm'
-      }
-    }
-  },
-  ssr: {
-    noExternal: true
+  const skin = env.SKIN_NAME || fs.readdirSync('./skins')[0]
+
+  const srcPath = fileURLToPath(new URL('./src', import.meta.url))
+
+  const commitIds = {
+    frontend: 'fedummy',
+    skin: 'skindummy'
   }
-}))
+  const versionHeader = crypto.createHash('md5')
+      .update(skin + JSON.stringify(commitIds))
+      .digest('hex')
+      .slice(0, 17)
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      vueDevTools(),
+      skinRawLoaderPlugin(skin),
+      ...(isSsrBuild ? [] : [
+        metadata({ skin, versionHeader, commitIds })
+      ])
+    ],
+    define: {
+      __THETREE_COMMIT_IDS__: JSON.stringify(commitIds),
+      __THETREE_VERSION_HEADER__: JSON.stringify(versionHeader)
+    },
+    base: process.env.NODE_ENV === 'production' ? `/skins/${skin}` : '/',
+    publicDir: isSsrBuild ? false : 'public',
+    resolve: {
+      extensions: ['.js', '.vue'],
+      alias: {
+        // vue: 'vue/dist/vue.esm-bundler.js',
+        '@': srcPath,
+        '~': srcPath,
+        'skin': path.resolve('./skins', skin),
+
+        'vuex': path.resolve(srcPath, 'main'),
+      },
+    },
+    server: {
+      proxy: {
+        '/internal': {
+          target: env.API_HOST || 'http://localhost:3000',
+          changeOrigin: true
+        }
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          format: isSsrBuild ? 'cjs' : 'esm'
+        }
+      }
+    },
+    ssr: {
+      noExternal: true
+    }
+  })
+})
