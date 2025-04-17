4748d02d615c8fd7140e699c788b8ad43464783b
HYONSU <admin@hyonsu.com>
2025-04-18 02:56:08 +0900

    폼 캡챠

--- src/components/form/seedForm.vue
+++ src/components/form/seedForm.vue
@@ -1,6 +1,7 @@
 <template>
   <form ref="form" v-bind="$attrs" @submit="submit">
     <slot/>
+    <div ref="captcha" v-if="useCaptcha" class="captcha"></div>
   </form>
 </template>
 <script>
@@ -13,7 +14,12 @@ export default {
   data() {
     return {
       submitting: false,
-      abortController: null
+      abortController: null,
+      useCaptcha: !this.$store.state.session.disable_captcha
+          && this.$store.state.config.captcha
+          && this.captcha,
+      captchaId: null,
+      captchaLock: []
     }
   },
   provide() {
@@ -24,10 +30,77 @@ export default {
   props: {
     captcha: Boolean
   },
+  computed: {
+    captchaConfig() {
+      return this.$store.state.config.captcha
+    }
+  },
+  mounted() {
+    if(this.useCaptcha) this.loadCaptcha()
+  },
+  watch: {
+    useCaptcha(newValue) {
+      if(newValue) this.loadCaptcha()
+    }
+  },
   methods: {
+    getCaptchaLib() {
+      return this.captchaConfig && {
+        recaptcha: window.grecaptcha,
+        turnstile: window.turnstile
+      }[this.captchaConfig.type]
+    },
+    async loadCaptcha() {
+      if(this.getCaptchaLib()) return
+
+      window.captchaOnLoad = async () => {
+        delete window.captchaOnLoad
+
+        this.captchaId = this.getCaptchaLib().render(this.$refs.captcha, {
+          sitekey: this.captchaConfig.site_key,
+          theme: this.$store.state.currentTheme,
+          callback: () => {
+            for(let { resolve } of this.captchaLock) resolve()
+            this.captchaLock.length = 0
+          },
+          ...{
+            recaptcha: {
+              badge: 'inline',
+              size: 'invisible'
+            },
+            turnstile: {
+              execution: 'execute'
+            }
+          }[this.captchaConfig.type]
+        })
+      }
+
+      const script = document.createElement('script')
+      script.src = {
+        recaptcha: 'https://www.google.com/recaptcha/api.js?render=explicit&onload=captchaOnLoad',
+        turnstile: 'https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit&onload=captchaOnLoad'
+      }[this.captchaConfig.type]
+      document.head.appendChild(script)
+    },
     async submit(e) {
       e.preventDefault()
       if(this.submitting) return
+
+      if(this.captchaId) {
+        for(let { reject } of this.captchaLock) reject()
+        this.captchaLock.length = 0
+
+        const captchaLib = this.getCaptchaLib()
+        captchaLib.reset(this.captchaId)
+        captchaLib.execute(this.captchaId)
+        try {
+          await new Promise((resolve, reject) => {
+            this.captchaLock.push({ resolve, reject })
+          })
+        } catch(e) {
+          return
+        }
+      }
 
       this.submitting = true
 
@@ -67,7 +140,7 @@ export default {
       }
       else if(typeof json.data === 'string') {
         if(json.data.includes('캡챠')) {
-          console.log('force enable captcha')
+          this.useCaptcha = true
         }
       }
 
@@ -85,3 +158,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+.captcha {
+  clear: both;
+}
+</style>
