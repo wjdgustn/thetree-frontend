d3af621ab24bc7e4911f29d7bd8e561e2c3fd2e6
HYONSU <admin@hyonsu.com>
2025-04-17 18:01:04 +0900

    NuxtLink ctrl 클릭 무시

--- src/components/global/nuxtLink.vue
+++ src/components/global/nuxtLink.vue
@@ -1,6 +1,6 @@
 <template>
   <RouterLink v-bind="$props" custom v-slot="{ href }">
-    <a ref="link" v-bind="$attrs" :href="href" @click.prevent="click"><slot/></a>
+    <a ref="link" v-bind="$attrs" :href="href" @click="click"><slot/></a>
   </RouterLink>
 </template>
 <script>
@@ -11,8 +11,11 @@ export default {
     ...RouterLink.props
   },
   methods: {
-    click() {
+    click(e) {
+      if(e.metaKey || e.ctrlKey || e.defaultPrevented) return
+
       this.$store.state.components.mainView.routerPush(this.$refs.link.getAttribute('href')).then()
+      e.preventDefault()
     }
   }
 }
