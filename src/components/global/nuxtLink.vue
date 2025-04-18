2eeafdca353dcf3fdc62b743a3ec1746edfa2cb5
HYONSU <admin@hyonsu.com>
2025-04-19 02:27:16 +0900

    NuxtLink query 직접 처리

--- src/components/global/nuxtLink.vue
+++ src/components/global/nuxtLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <RouterLink v-bind="$props" custom v-slot="{ href }">
+  <RouterLink v-bind="$props" :to="actualTo" custom v-slot="{ href }">
     <a ref="link" v-bind="$attrs" :href="href" @click="click"><slot/></a>
   </RouterLink>
 </template>
@@ -9,6 +9,22 @@ import { RouterLink } from 'vue-router';
 export default {
   props: {
     ...RouterLink.props
+  },
+  data() {
+    return {
+      actualTo: this.to
+    }
+  },
+  created() {
+    if(typeof this.actualTo === 'string') return
+
+    // vue-router 자체 router.resolve는 path를 이상하게 encode하는 듯
+    const url = new URL(this.$route.fullPath, 'https://example.com')
+    for(let [key, value] of Object.entries(this.actualTo.query)) {
+      if(value === null) url.searchParams.delete(key)
+      else url.searchParams.set(key, value)
+    }
+    this.actualTo= url.pathname + url.search
   },
   methods: {
     click(e) {
