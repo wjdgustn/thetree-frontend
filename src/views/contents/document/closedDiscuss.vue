15af05f5debd29bb4e1e6add4658d87071f43a17
정현수 <admin@hyonsu.com>
2025-04-23 10:18:29 +0900

    닫힌 편집 요청, 닫힌 토론 목록

---
+++ src/views/contents/document/closedDiscuss.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in viewData.threads">
+      {{index + 1}}.
+      <NuxtLink :to="'/thread/' + item.url" v-text="item.topic"/>
+    </li>
+  </ul>
+</template>
+<script>
+import NuxtLink from '@/components/global/nuxtLink'
+
+export default {
+  components: {
+    NuxtLink
+  }
+}
+</script>
+<style scoped>
+ul {
+  margin: .4em 0 .4em 1.5em;
+  padding-left: .5rem;
+}
+
+ul li {
+  list-style: inherit !important;
+  margin: .4em 0;
+}
+</style>
---
+++ src/views/contents/document/closedEditRequest.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="item in viewData.editRequests">
+      <NuxtLink :to="'/edit_request/' + item.url">편집 요청 {{item.url}}</NuxtLink>
+    </li>
+  </ul>
+  <pre>{{viewData}}</pre>
+</template>
+<script>
+import NuxtLink from '@/components/global/nuxtLink'
+
+export default {
+  components: {
+    NuxtLink
+  }
+}
+</script>
+<style scoped>
+ul {
+  margin: .4em 0 .4em 1.5em;
+  padding-left: .5rem;
+}
+
+ul li {
+  list-style: inherit !important;
+  margin: .4em 0;
+}
+</style>
