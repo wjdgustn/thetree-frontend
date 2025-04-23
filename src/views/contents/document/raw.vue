4058e4f3c32fca9faa3b63511635b820cbc3b693
HYONSU <admin@hyonsu.com>
2025-04-24 07:16:37 +0900

    raw 페이지

---
+++ src/views/contents/document/raw.vue
@@ -0,0 +1,28 @@
+<template>
+  <textarea readonly :value="viewData.content"/>
+</template>
+<style scoped>
+textarea {
+  border: .0625rem solid #ccc;
+  border-radius: 0;
+  color: #55595c;
+  font-family: inherit;
+  font-size: .9rem;
+  height: 600px;
+  line-height: 1.5;
+  margin: 0;
+  overflow: auto;
+  padding: .25rem .5rem;
+  width: 100%
+}
+
+textarea[readonly] {
+  background-color: #eceeef;
+  opacity: 1
+}
+
+.theseed-dark-mode textarea {
+  background-color: #000;
+  color: #ddd
+}
+</style>
