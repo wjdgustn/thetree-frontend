99ca6c37be56b58d332708dfafbe0dc8cb6cd8b2
hyonsu <admin@hyonsu.com>
2025-04-25 12:26:06 +0900

    편집된 지 오래된 문서 페이지

---
+++ src/views/contents/docList/OldPages.vue
@@ -0,0 +1,40 @@
+<template>
+  <p>편집된 지 오래된 문서의 목록입니다. (리다이렉트 제외)</p>
+  <PrevNextBtn flex :="data.pageProps"/>
+  <ul>
+    <li v-for="item in data.items">
+      <NuxtLink :to="doc_action_link(item.document.parsedName, 'w')">
+        {{doc_fulltitle(item.document.parsedName)}}
+      </NuxtLink>
+      (수정 시각: <LocalDate :date="item.createdAt"/>)
+    </li>
+  </ul>
+  <PrevNextBtn flex :="data.pageProps"/>
+</template>
+<script>
+import Common from '@/mixins/common'
+import PrevNextBtn from '@/components/prevNextBtn'
+import NuxtLink from '@/components/global/nuxtLink'
+import LocalDate from '@/components/localDate'
+
+export default {
+  mixins: [Common],
+  components: {
+    LocalDate,
+    NuxtLink,
+    PrevNextBtn
+  }
+}
+</script>
+<style scoped>
+ul {
+  margin: .4em 0 .4em 1.5em;
+  padding-left: .5rem;
+}
+
+ul>li {
+  font-size: .9rem;
+  list-style: inherit !important;
+  margin: .4em 0;
+}
+</style>
