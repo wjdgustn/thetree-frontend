50b0a020857dccee481d2a0430d207f4f9327372
HYONSU <admin@hyonsu.com>
2025-04-27 01:50:38 +0900

    settingItemInput 컴포넌트

---
+++ src/components/settingItemInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <SettingItem>
+    <input v-model="value" :id="ckey" @change="changed" :type="type" placeholder="입력">
+  </SettingItem>
+</template>
+<script>
+import SettingItem from '@/components/settingItem'
+
+export default {
+  extends: SettingItem,
+  components: {
+    SettingItem
+  },
+  props: {
+    type: {
+      type: String,
+      default: 'text'
+    },
+    default: {
+      type: String
+    }
+  },
+  methods: {
+    changed() {
+      this.$store.state.localConfigSetValue(this.ckey, this.value)
+    }
+  }
+}
+</script>
+<style scoped>
+input {
+  background-color: #fff;
+  background-image: none;
+  border: .0625rem solid #ccc;
+  color: #55595c;
+  display: block;
+  line-height: 1.5;
+  margin: 0;
+  max-width: 16rem;
+  padding: .375rem .75rem;
+  width: 100%;
+}
+</style>
