c25d9564cc287c97a36052ea3ad726b552b0c672
HYONSU <admin@hyonsu.com>
2025-04-28 22:47:34 +0900

    이메일 화이트리스트가 비어있는 경우 숨기기 close wjdgustn/thetree#55

--- src/components/emailWhitelist.vue
+++ src/components/emailWhitelist.vue
@@ -1,11 +1,13 @@
 <template>
-  <p>
-    이메일 허용 목록이 활성화 되어 있습니다.
-    <br>이메일 허용 목록에 존재하는 메일만 사용할 수 있습니다.
-  </p>
-  <ul>
-    <li v-for="domain in domains" v-text="domain"></li>
-  </ul>
+  <template v-if="domains.length">
+    <p>
+      이메일 허용 목록이 활성화 되어 있습니다.
+      <br>이메일 허용 목록에 존재하는 메일만 사용할 수 있습니다.
+    </p>
+    <ul>
+      <li v-for="domain in domains" v-text="domain"></li>
+    </ul>
+  </template>
 </template>
 <script>
 export default {
