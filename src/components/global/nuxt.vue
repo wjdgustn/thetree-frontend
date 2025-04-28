dd4b9e95f0fa01bd4677db3e6e2bc897433fb2a4
HYONSU <admin@hyonsu.com>
2025-04-28 10:32:19 +0900

    contentHtml onDynamicContentClick 핸들러 추가

--- src/components/global/nuxt.vue
+++ src/components/global/nuxt.vue
@@ -1,4 +1,11 @@
 <template>
-  <div v-if="$store.state.page.contentHtml" v-html="$store.state.page.contentHtml"></div>
+  <div v-if="$store.state.page.contentHtml" v-html="$store.state.page.contentHtml" @click="onDynamicContentClick"></div>
   <component v-else :is="$store.state.viewData.viewComponent"></component>
 </template>
+<script>
+import Common from '@/mixins/common'
+
+export default {
+  mixins: [Common]
+}
+</script>
