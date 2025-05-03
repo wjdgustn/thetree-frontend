9688d699f46606a4cc365b2e15bc41e9e7639afc
HYONSU <admin@hyonsu.com>
2025-05-04 04:21:35 +0900

    알림이 없을 시 모두 읽음 버튼 비활성화

--- src/views/contents/member/notifications.vue
+++ src/views/contents/member/notifications.vue
@@ -18,7 +18,7 @@
   ]"/>
   <div class="top-button-group">
     <PrevNextBtn flex v-bind="data.pageProps"/>
-    <GeneralButton :whenClick="readAll">모두 읽음</GeneralButton>
+    <GeneralButton :whenClick="readAll" :disabled="!data.items.length">모두 읽음</GeneralButton>
   </div>
   <ul class="list">
     <li v-for="item in data.items" class="row-parent" :class="{ read: item.read }">
