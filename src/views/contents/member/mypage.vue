fe6f2c49ef8bacf7893a6cd22d6ba6ee6e4f46d9
HYONSU <admin@hyonsu.com>
2025-05-05 00:29:41 +0900

    버튼 색상 변경

--- src/views/contents/member/mypage.vue
+++ src/views/contents/member/mypage.vue
@@ -78,7 +78,7 @@
 
     <SeedFormBlock v-if="data.permissions.includes('engine_developer')" label="엔진 개발자">
       <SeedButton v-if="data.permissions.includes('developer')" type="button" danger @click="removeDeveloperPerm">개발자 권한 제거</SeedButton>
-      <SeedButton v-else type="button" danger @click="getDeveloperPerm">개발자 권한 받기</SeedButton>
+      <SeedButton v-else type="button" submit @click="getDeveloperPerm">개발자 권한 받기</SeedButton>
     </SeedFormBlock>
 
     <div class="button-block">
