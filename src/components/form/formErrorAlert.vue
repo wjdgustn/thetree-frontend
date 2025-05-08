ca8ae2a8c01efada5b75450dcb858316841a9bb3
hyonsu <admin@hyonsu.com>
2025-05-08 15:13:09 +0900

    formErrorAlert 텍스트 영역 span 처리 close wjdgustn/thetree#67

--- src/components/form/formErrorAlert.vue
+++ src/components/form/formErrorAlert.vue
@@ -1,5 +1,8 @@
 <template>
-  <Alert v-if="errorAlert" error v-html="'<b>[오류!]</b>&nbsp;' + errorAlert" class="error-alert"/>
+  <Alert v-if="errorAlert" error class="error-alert">
+    <b>[오류!]</b>
+    &nbsp;<span v-html="errorAlert"/>
+  </Alert>
 </template>
 <script>
 import Alert from '@/components/alert'
