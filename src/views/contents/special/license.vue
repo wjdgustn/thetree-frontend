c1244f68a9394f3b078a253d4ec9997c8fdc3740
HYONSU <admin@hyonsu.com>
2025-06-30 13:05:35 +0900

    커밋 ID 호버 시 업데이트 날짜 표시

--- src/views/contents/special/license.vue
+++ src/views/contents/special/license.vue
@@ -4,9 +4,9 @@
   <p>v{{version}}<span v-if="branch"> ({{branch}})</span></p>
   <p>엔진 업데이트: <LocalDate :date="commitDate"/> (<LocalDate :date="commitDate" forceRelative/>)</p>
   <ul>
-    <li>Backend: {{commitId}}</li>
-    <li>Frontend: {{commitIds.frontend}}</li>
-    <li>Skin: {{commitIds.skin}}</li>
+    <li :title="beDate">Backend: {{commitId}}</li>
+    <li :title="feDate">Frontend: {{commitIds.frontend}}</li>
+    <li :title="skinDate">Skin: {{commitIds.skin}}</li>
   </ul>
 
   <p>Copyright <a href="https://github.com/wjdgustn">hyonsu</a> all rights reserved.</p>
@@ -26,6 +26,8 @@
 <script>
 import LocalDate from '@/components/localDate'
 
+const toDateStr = date => new Date(date).toISOString().slice(0, 19).replace('T', ' ')
+
 export default {
   components: {
     LocalDate
@@ -35,6 +37,17 @@ export default {
       ...this.$store.state.viewData,
       commitIds: __THETREE_COMMIT_IDS__,
       commitDates: __THETREE_COMMIT_DATES__
+    }
+  },
+  computed: {
+    beDate() {
+      return toDateStr(this.commitDate)
+    },
+    feDate() {
+      return toDateStr(this.commitDates.frontend)
+    },
+    skinDate() {
+      return toDateStr(this.commitDates.skin)
     }
   }
 }
