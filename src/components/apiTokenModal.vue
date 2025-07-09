7f0a9fd8dd33c7a1565110bc96dd897408e0a973
hyonsu <admin@hyonsu.com>
2025-07-09 10:32:37 +0900

    API Token 신규 UI

--- src/components/apiTokenModal.vue
+++ src/components/apiTokenModal.vue
@@ -1,39 +1,61 @@
 <template>
   <Modal @closed="closed" v-slot="props">
-    <SeedForm method="post" action="/member/generate_api_token">
-      <h4>API Token 발급</h4>
+    <div class="modal-block">
+      <div class="modal-title">API Token 발급</div>
       <FormErrorAlert/>
-
-      <template v-if="apiToken">
-        <SeedFormBlock label="토큰:">
-          <SeedFormInput readonly :value="apiToken" @click="selectAll"/>
-        </SeedFormBlock>
-        <ul>
-          <li>발급된 토큰은 이 창을 닫으면 다시 확인할 수 없습니다.</li>
-          <li>토큰은 비밀번호와 같이 취급해주세요.</li>
-        </ul>
-      </template>
-      <SeedFormBlock v-else label="비밀번호:" name="password">
-        <SeedFormInput type="password" name="password"/>
-      </SeedFormBlock>
-
-      <div class="modal-button-block">
-        <SeedButton v-if="!apiToken" danger large>생성</SeedButton>
-        <SeedButton type="button" large @click="props.close" v-text="apiToken ? '닫기' : '취소'"/>
-      </div>
-    </SeedForm>
+      <SeedForm method="post" action="/member/generate_api_token" :class="[$style.form, $style['form--full']]">
+        <template v-if="apiToken">
+          <div :class="$style.form__row">
+            <InputField multiline readonly class="token-textarea" :value="apiToken"/>
+          </div>
+          <div :class="$style.form__row">
+            <ul :class="$style.list">
+              <li>발급된 토큰은 이 창을 닫으면 다시 확인할 수 없습니다.</li>
+              <li>토큰은 비밀번호와 같이 취급해주세요.</li>
+            </ul>
+          </div>
+          <div :class="[$style.form__row, $style['form__row--buttons']]">
+            <div :class="$style.form__buttons">
+              <GeneralButton :whenClick="copyToken">토큰 복사</GeneralButton>
+              <GeneralButton :class="$style.button" :whenClick="props.close">닫기</GeneralButton>
+            </div>
+          </div>
+        </template>
+        <template v-else>
+          <div :class="$style.form__row">
+            <label for="passwordInput">비밀번호</label>
+            <InputField type="password" id="passwordInput" name="password"/>
+            <p :class="[$style.text, $style['text--help']]">
+              사용자 계정의 비밀번호를 입력하세요.
+            </p>
+          </div>
+          <div :class="[$style.form__row, $style['form__row--buttons']]">
+            <div :class="$style.form__buttons">
+              <GeneralButton :class="$style.button" :whenClick="props.close">취소</GeneralButton>
+              <GeneralButton :class="$style.button" type="submit" theme="primary">발급</GeneralButton>
+            </div>
+          </div>
+        </template>
+      </SeedForm>
+    </div>
   </Modal>
 </template>
 <script>
+import { toast } from 'vue-sonner'
+
 import FormErrorAlert from '@/components/form/formErrorAlert'
 import SeedButton from '@/components/seedButton'
 import SeedForm from '@/components/form/seedForm'
 import SeedFormBlock from '@/components/form/seedFormBlock'
 import SeedFormInput from '@/components/form/seedFormInput'
 import Modal from '@/components/modal'
+import InputField from '@/components/form/inputField'
+import GeneralButton from '@/components/generalButton'
 
 export default {
   components: {
+    GeneralButton,
+    InputField,
     Modal,
     FormErrorAlert,
     SeedButton,
@@ -50,44 +72,31 @@ export default {
     closed() {
       this.$store.state.viewData.apiToken = null
     },
-    selectAll(e) {
-      e.target.setSelectionRange(0, e.target.value.length)
+    copyToken() {
+      navigator.clipboard.writeText(this.apiToken)
+      toast('API 토큰이 복사되었습니다.')
     }
   }
 }
 </script>
+<style module>
+@import '@/styles/form.css';
+</style>
 <style scoped>
-form {
-  box-sizing: border-box;
-  flex: 1 0 auto;
-  font-size: 14px;
-  padding: 15px;
+.modal-block {
+  padding: 1rem;
   width: 100%;
 }
 
-form h4 {
-  font-weight: 600;
-  margin: 0;
-  padding: 0;
+.modal-title {
+  font-size: 1.05rem;
+  font-weight: 500;
+  margin: 0 0 1rem;
 }
 
-form div {
-  margin-bottom: 0;
-}
-
-form :deep(div label) {
-  margin-block-start: 1em;
-  margin-bottom: 0;
-}
-
-form .seed-form-input {
-  margin: 0;
-  width: 100%;
-}
-
-.modal-button-block {
-  float: right;
-  margin-bottom: 1rem;
-  margin-top: 1rem;
+.token-textarea {
+  height: 5rem;
+  resize: none;
+  word-break: break-all;
 }
 </style>
