1f8cf6b934f7dd3ad24b00cb7dc86c50f7999a2c
HYONSU <admin@hyonsu.com>
2026-03-02 02:22:31 +0900

    기본 viewport 변경

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -54,7 +54,7 @@ export default {
       meta: [
         {
           name: 'viewport',
-          content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, user-scalable=no'
+          content: 'initial-scale=1.0, minimum-scale=1.0, width=device-width'
         },
         { name: 'generator', content: 'the tree' },
         { name: 'mobile-web-app-capable', content: 'yes' },
